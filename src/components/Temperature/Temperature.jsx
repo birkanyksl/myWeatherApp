01661a9bb101d589bd620b56047a7a8ad0bd6af5
Birkan <birkanyksl613@gmail.com>
2024-04-07 06:47:27 +0300

    css değişikliği

--- src/components/Temperature/Temperature.jsx
+++ src/components/Temperature/Temperature.jsx
@@ -5,23 +5,30 @@ const Temperature = ({ weatherData }) => {
     return <div>Loading...</div>;
   }
 
+  const iconBaseUrl = "http://openweathermap.org/img/wn/";
+  const iconCode = weatherData.weather[0].icon;
+  const iconUrl = `${iconBaseUrl}${iconCode}@2x.png`;
+
   return (
     <div className="temperature-container">
       <p className="day-time-container">
         <span>15.03.2024</span>
         <span>14.30</span>
       </p>
+
       <p className="city-name">
         <span>{weatherData?.name}</span>
       </p>
+
       <p className="temp">8°C</p>
 
       <div className="weather-info-container">
         <div className="weather-info">
-          <span>icon</span>
+          <img src={iconUrl} alt="Weather Icon" />
           <p className="description">{weatherData.weather[0].description}</p>
         </div>
-        <p>
+
+        <p className="daily-low-high">
           <span>Low: 8°C</span>
           <span>High: 12°C</span>
         </p>
