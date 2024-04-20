f850025117315ffe3d00b3b477f5e5195c1f3332
Birkan <birkanyksl613@gmail.com>
2024-04-20 04:43:37 +0300

    config update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/myWeatherApp/",
   plugins: [react()],
+  assetsInclude: ["**/*.png", "**/*.ico"],
 });
